ad953e097e09d1392015599eedf088244397b515
4aKBoris <nagibpopolnoy@mail.ru>
2021-11-28 22:32:54 +0300

    Главные окна приложения

--- app/src/main/java/com/example/messenger/navigation/screens/MainScreens.kt
+++ app/src/main/java/com/example/messenger/navigation/screens/MainScreens.kt
@@ -1,8 +1,15 @@
 package com.example.messenger.navigation.screens
 
-sealed class MainScreens: Screen {
+import com.example.messenger.data.LoginData
+import com.example.messenger.data.User
+import com.example.messenger.navigation.DATA
+import com.example.messenger.navigation.USER
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-    object Welcome: MainScreens() {
+sealed class MainScreens : Screen {
+
+    object Welcome : MainScreens() {
 
         override val name: String
             get() = "welcome"
@@ -13,7 +20,7 @@ sealed class MainScreens: Screen {
         fun createRoute() = route
     }
 
-    object Authorization: MainScreens() {
+    object Authorization : MainScreens() {
 
         override val name: String
             get() = "authorization"
@@ -24,7 +31,7 @@ sealed class MainScreens: Screen {
         fun createRoute() = route
     }
 
-    object Registration: MainScreens() {
+    object Registration : MainScreens() {
 
         override val name: String
             get() = "registration"
@@ -35,15 +42,34 @@ sealed class MainScreens: Screen {
         fun createRoute() = route
     }
 
-    object Chat: MainScreens() {
+    object Chat : MainScreens() {
 
         override val name: String
             get() = "chat"
 
         override val route: String
-            get() = "id/{id}/$name"
+            get() = "$DATA/{$DATA}/$name"
 
-        fun createRoute(id: Int) = "id/$id/$name"
+        fun createRoute(data: LoginData) =
+            "$DATA/${Json.encodeToString(data)}/$name"
     }
 
+    object SettingsData: MainScreens() {
+        override val name: String
+            get() = "settingsData"
+        override val route: String
+            get() = "$USER/{$USER}/$name"
+
+        fun createRoute(user: User) =
+            "$USER/${Json.encodeToString(user)}/$name"
+    }
+
+    object SettingsPassword: MainScreens() {
+        override val name: String
+            get() = "settingsPassword"
+        override val route: String
+        get() = name
+
+        fun createRoute() = route
+    }
 }
