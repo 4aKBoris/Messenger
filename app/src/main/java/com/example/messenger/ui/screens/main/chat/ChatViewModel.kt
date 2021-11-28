575b97a17f6b9ee615004e2e3978d1be72d0f08e
4aKBoris <nagibpopolnoy@mail.ru>
2021-11-28 22:26:31 +0300

    Основное окно чата

--- app/src/main/java/com/example/messenger/ui/screens/main/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/messenger/ui/screens/main/chat/ChatViewModel.kt
@@ -6,14 +6,21 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
 import com.example.messenger.data.ChatUser
+import com.example.messenger.data.LoginData
 import com.example.messenger.data.Message
+import com.example.messenger.data.User
 import com.example.messenger.exception.MessengerException
+import com.example.messenger.navigation.screens.MainScreens
+import com.example.messenger.navigation.screens.PasswordScreens
 import com.example.messenger.network.Requests
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.datetime.toKotlinLocalDateTime
+import java.net.ConnectException
 import java.time.LocalDateTime
 
 class ChatViewModel : ViewModel() {
@@ -24,6 +31,9 @@ class ChatViewModel : ViewModel() {
     private val _users = mutableStateOf(listOf<ChatUser>())
     private val _messages = mutableStateOf(listOf<Message>())
     private val _scroll = mutableStateOf(false)
+    private val _dialogSettings = mutableStateOf(false)
+    private val _dialogAbout = mutableStateOf(false)
+    private val _dialogInfo = mutableStateOf(false)
 
     val message: State<String> = _message
     val dialogState: State<Boolean> = _dialogState
@@ -31,6 +41,58 @@ class ChatViewModel : ViewModel() {
     val users: State<List<ChatUser>> = _users
     val messages: State<List<Message>> = _messages
     val scroll: State<Boolean> = _scroll
+    val dialogSettings: State<Boolean> = _dialogSettings
+    val dialogAbout: State<Boolean> = _dialogAbout
+    val dialogInfo: State<Boolean> = _dialogInfo
+
+    fun onChangeDialogSettings(state: Boolean) {
+        _dialogSettings.value = state
+    }
+
+    fun onChangeDialogAbout(state: Boolean) {
+        _dialogAbout.value = state
+    }
+
+    fun onChangeDialogInfo(state: Boolean) {
+        _dialogInfo.value = state
+    }
+
+    suspend fun getUserInfo(data: LoginData) = Requests.getUserInfo(data = data)
+
+    fun onDelete(loginData: LoginData, navController: NavController) =
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val flag = Requests.deleteUser(data = loginData)
+                if (!flag) throw MessengerException("Не удалось удалить профиль! Попробуйте ещё раз")
+                onChangeDialogSettings(false)
+                withContext(Dispatchers.Main) {
+                    navController.backQueue.clear()
+                    navController.navigate(MainScreens.Welcome.createRoute())
+                }
+            } catch (e: MessengerException) {
+                _dialogState.value = true
+                _error.value = e.message!!
+            } catch (e: ConnectException) {
+                _dialogState.value = true
+                _error.value = "Не удалось устаность соединение с сервером! Попробуйте ещё раз"
+            }
+        }
+
+    fun onPasswordChangeNavigation(
+        navController: NavController,
+        data: LoginData
+    ) {
+        navController.navigate(PasswordScreens.OldPassword.createRoute(data = data))
+        onChangeDialogSettings(state = false)
+    }
+
+    fun onDataChangeNavigation(
+        navController: NavController,
+        user: User
+    ) {
+        navController.navigate(MainScreens.SettingsData.createRoute(user = user.copy(dataUser = user.dataUser.copy(icon = null))))
+        onChangeDialogSettings(state = false)
+    }
 
     fun onChangeMessage(message: String) {
         _message.value = message
@@ -54,8 +116,6 @@ class ChatViewModel : ViewModel() {
             delay(5000L)
         }
     }
-
-    fun getUserName(id: Int) = _users.value.first { it.id == id }.getName()
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun onSendClick(id: Int) = viewModelScope.launch(Dispatchers.IO) {
